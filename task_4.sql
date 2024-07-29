3170fa51141e5639426f255c015b9aef3e313315
itsmearafik <arafikalhassan@gmail.com>
2024-07-29 19:36:03 +0100

    Completed week 7 tasks of the ALX introduction to databses using MySQL.

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- Select the database
+USE alx_book_store;
+
+-- Query information_schema to get the full description of the books table
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
